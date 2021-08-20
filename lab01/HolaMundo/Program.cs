cc823cc239be35a08f46da30cd27fa07d3916c34
Joseph Antonio <joseph.carita@tecsup.edu.peclear>
2021-08-20 13:20:30 -0500

    Lab01 - parte 1

---
+++ lab01/HolaMundo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola, cual es tu nombre?");
+            String name = Console.ReadLine();
+            Console.WriteLine("Encantado de conocerte," + name + " !!!");
+        }
+    }
+}
