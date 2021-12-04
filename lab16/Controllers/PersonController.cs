00c8af545e79d68be3795266a11bdd5e7b195e0a
Joseph Antonio <joseph.carita@tecsup.edu.pe>
2021-12-04 15:23:52 -0500

    Lab16 - completed

---
+++ lab16/Controllers/PersonController.cs
@@ -0,0 +1,33 @@
+using person.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace persons.Controllers
+{
+   [ApiController]
+   [Route("[controller]")]
+    public class PersonController: ControllerBase 
+    {
+     private readonly PersonContext _context;
+     public PersonController(PersonContext context )
+     {
+      _context = context;
+     }
+
+     [HttpGet]
+     public IEnumerable<Person> GetAll()
+     {
+      return _context.Person.ToList();
+     }
+
+     [HttpGet("{id}")]
+     public async Task<ActionResult<Person>> GetById(int id)
+     {
+      var item = await _context.Person.FindAsync(id);
+      if(item == null)
+      return NotFound();
+
+      return item;
+
+     }
+    }
+}
